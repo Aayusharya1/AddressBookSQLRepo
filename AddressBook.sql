ca7f60159bb8309d5b0a879f00ff59b5b9b243f5
aayusharya1 <aayusharya07@gmail.com>
2020-11-02 16:45:22 +0530

    [Aayush] Add. Query for creating AddressBook table

--- AddressBook.sql
+++ AddressBook.sql
@@ -8,3 +8,17 @@ create database Address_Book_Service
 --Using AddressBook Service database
 use Address_Book_Service
 
+--Creating Addressbook table
+create table AddressBook
+(
+FirstName varchar(20) not null,
+LastName varchar (20) not null,
+Address varchar(50) not null,
+City varchar(15) not null,
+State varchar(15) not null,
+Zipcode varchar(10) not null,
+Phone varchar(15) not null,
+Email varchar(20) not null 
+);
+
+
